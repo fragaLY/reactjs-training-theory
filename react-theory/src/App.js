ee610d6658c748cf904c50061a3a367e35522dc6
Vadzim Kavalkou <vadzim.kavalkou@gmail.com>
2020-11-10 15:57:35 +0300

    Chapter3 (#3)
    * Chapter 3:
    Adding a state
    
    * Chapter 3:
    Passing parameters in a function.
    
    * Chapter 3:
    Input handling
    
    * Lesson 3: Case handling
    
    * Lesson 3: Dynamic lists

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -1,17 +1,58 @@
-import React, {Component} from 'react';
+import {Component} from 'react';
 import './App.css';
 import Car from './Car/Car'
 
 class App extends Component {
+
+    state = {
+        cars: [
+            {name: 'Ford', year: 2002},
+            {name: 'Lincoln', year: 2003},
+            {name: 'Zaporozec', year: 2004},
+            {name: 'Mazda', year: 2005}
+        ],
+        title: 'REACTIVE CARS, YEAH!',
+        showCars: false
+    }
+
+    changeName = (name, index) => {
+        const car = this.state.cars[index]
+        car.name = name
+        const cars = [...this.state.cars] // spread operator for duplicating the data
+        cars[index] = car
+        this.setState({ cars })
+    }
+
+    toggle = () => { // doesn't creates its context and works with previous one
+        this.setState({ showCars: !this.state.showCars})
+    }
+
+    delete(index) { // uses context of parent
+        let cars = [...this.state.cars] // spread operator for duplicating the data
+        cars.splice(index, 1)
+        this.setState({ cars })
+    }
+
     render() {
+        let cars = null
+
+        if (this.state.showCars) {
+            cars = this.state.cars.map((car, index) => {
+                return (
+                    <Car key={index}
+                         name={car.name}
+                         year={car.year}
+                         onChangeName={ event => this.changeName(event.target.value, index)}
+                         onDelete={ this.delete.bind(this, index) }
+                    />
+                )
+            })
+        }
         return (
             <div className="App">
-                <h1>Cars</h1>
-                <Car name = {'Ford'} year={2018}/>
-                <Car name = {'Audi'} year={2020}/>
-                <Car name = {'Mazda'} year={2015}>
-                    <p style={{color: 'red'}}>COLOR</p>
-                </Car>
+                <h1>{this.state.title}</h1>
+                <button onClick={ this.toggle }>Toggle Cars</button>
+                { cars }
             </div>
         )
     }
--- react-theory/src/Car/Car.js
+++ react-theory/src/Car/Car.js
@@ -1,8 +1,12 @@
 export default props => (
-    <div>
-        <h1>Car</h1>
+    <div style={{
+            border: '1px solid #ccc',
+            marginBottom: '10px',
+            padding: '10px'
+    }}>
         <p>Name: {props.name}</p>
         <p>Year: {props.year}</p>
-        { props.children }
+        <input type="text" onChange={props.onChangeName} value={props.name}/>
+        <button onClick={props.onDelete}>Delete</button>
     </div>
 )
