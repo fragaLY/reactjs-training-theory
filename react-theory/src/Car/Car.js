2d7618f04f703556f8539cc718d815f06eb2ac24
fragaly <vadzim.kavalkou@gmail.com>
2020-11-10 16:20:55 +0300

    Dynamic classes

--- react-theory/src/Car/Car.js
+++ react-theory/src/Car/Car.js
@@ -1,10 +1,29 @@
 import './Car.css'
 
-export default props => (
-    <div className="Car">
-        <p>Name: {props.name}</p>
-        <p>Year: {props.year}</p>
-        <input type="text" onChange={props.onChangeName} value={props.name}/>
-        <button onClick={props.onDelete}>Delete</button>
-    </div>
-)
+export default props => {
+    const inputClasses = ['input']
+
+    if (props.name !== '') {
+        inputClasses.push('green')
+    } else {
+        inputClasses.push('red')
+    }
+
+    if (props.name.length > 4) {
+        inputClasses.push('bold')
+    }
+
+    return (
+        <div className="Car">
+            <p>Name: {props.name}</p>
+            <p>Year: {props.year}</p>
+            <input
+                type="text"
+                onChange={props.onChangeName}
+                value={props.name}
+                className={inputClasses.join(' ')}
+            />
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+    )
+}
