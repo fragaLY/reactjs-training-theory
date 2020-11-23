915381d2db26325300769700a13b4496f28f7d4b
fragaly <vadzim.kavalkou@gmail.com>
2020-11-23 23:21:30 +0300

    lecture 3-6: lifecycle

--- react-theory/src/App.js
+++ react-theory/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
 
     constructor(props) {
         super(props);
+        console.log("Constructor")
         this.state = {
             cars: [
                 {name: 'Ford', year: 2002},
@@ -23,26 +24,68 @@ class App extends Component {
         car.name = name
         const cars = [...this.state.cars] // spread operator for duplicating the data
         cars[index] = car
-        this.setState({ cars })
+        this.setState({cars})
     }
 
     toggle = () => { // doesn't creates its context and works with previous one
-        this.setState({ showCars: !this.state.showCars})
+        this.setState({showCars: !this.state.showCars})
     }
 
     delete(index) { // uses context of parent
         let cars = [...this.state.cars] // spread operator for duplicating the data
         cars.splice(index, 1)
-        this.setState({ cars })
+        this.setState({cars})
     }
 
+    // legacy , set state in constructor
+    // componentWillMount() {
+    //     console.log("Component will mount")
+    // }
 
-    componentWillMount() {
-        console.log("Component will mount")
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log("Get snapshot before update")
+        // https://stackoverflow.com/questions/58032963/getderivedstatefromprops-returned-undefined
+        // return {
+        //     currentUser: {
+        //         ...state.currentUser,
+        //         id: props.location.state && props.location.state.user ? props.location.state.user.id : state.currentUser.id,
+        //         name: props.location.state && props.location.state.user ? props.location.state.user.name : state.currentUser.name,
+        //         roles: props.location.state && props.location.state.user ? props.location.state.user.roles : state.currentUser.roles,
+        //         hasAuthenticated: true;
+        //     }
+        // }
     }
 
     componentDidMount() {
         console.log("Component did mount")
+    }
+
+    // legacy. use getDerivedStateFromProps to prevent changing states in this methods
+    // componentWillReceiveProps(nextProps, nextContext) {
+    //     console.log("Component will receive next props", nextProps)
+    // }
+
+    // legacy use getDerivedStateFromProps to prevent changing states in this methods
+    // componentWillUpdate(nextProps, nextState, nextContext) {
+    //     console.log("Component will update.", nextProps, nextState)
+    // }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        console.log("Get derived state from props.", nextProps, prevState)
+        return prevState
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log("Should component update.", nextProps, nextState)
+        return true
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("Component did update.",  prevProps,  prevState)
+    }
+
+    componentWillUnmount() {
+        console.log("Component will unmount")
     }
 
     render() {
@@ -56,8 +99,8 @@ class App extends Component {
                     <Car key={index}
                          name={car.name}
                          year={car.year}
-                         onChangeName={ event => this.changeName(event.target.value, index)}
-                         onDelete={ this.delete.bind(this, index) }
+                         onChangeName={event => this.changeName(event.target.value, index)}
+                         onDelete={this.delete.bind(this, index)}
                     />
                 )
             })
@@ -66,13 +109,13 @@ class App extends Component {
             <div className="App">
                 {/*<h1>{this.state.title}</h1>*/}
                 <h1>{this.props.title}</h1>
-                <button onClick={ this.toggle }>Toggle Cars</button>
+                <button onClick={this.toggle}>Toggle Cars</button>
                 <div style={{
                     width: 400,
                     margin: 'auto',
                     paddingTop: '20px'
                 }}>
-                    { cars }
+                    {cars}
                 </div>
             </div>
         )
